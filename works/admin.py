475411a78f64720c99bb9e6e7443aa179ba2bc5a
lu5her <lushersavior@gmail.com>
2021-06-23 20:08:45 +0700

    modified db

--- works/admin.py
+++ works/admin.py
@@ -4,4 +4,10 @@ from .models import Work
 
 # Register your models here.
 
-admin.site.register(Work)
+
+
+class WorkList(admin.ModelAdmin):
+	model = Work
+	list_display = ['name', 'type', 'worker', 'work_date']
+	
+admin.site.register(Work, WorkList)
