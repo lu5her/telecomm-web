dc7bc058dd8ff8a3d605b7fd78ec6f19ce319cbe
lu5her <lushersavior@gmail.com>
2021-06-25 20:08:56 +0700

    add ckeditor

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 #from django.contrib.auth.models import Employee
 from django.urls import reverse_lazy
+from ckeditor.fields import RichTextField
 
 from accounts.models import Employee
 
@@ -16,7 +17,8 @@ class News(models.Model):
 	type = models.CharField(max_length=50, choices=TYPES, default='สั่งการ')
 	date_published = models.DateField(auto_now_add=True)
 	name = models.CharField(max_length=200)
-	detail = models.TextField()
+	#detail = models.TextField()
+	detail = RichTextField()
 	author = models.ForeignKey(Employee, on_delete=models.CASCADE)
 	read = models.ManyToManyField(Employee, related_name='read_newss')
 
